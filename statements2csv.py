c74e83078630575fcdc5305eedcb507d352fbc0c
John Kurkowski <john.kurkowski@gmail.com>
2021-10-14 20:43:12 -0700

    Skip duplicate tables
    Camelot extracts the same table twice in a row. Not sure why. Duplicates seem to occur next to each other. Check if the previous and current dataframes are equal. Can't use `==` as that is ambiguous, according to Pandas. Can't use a set, as dataframes aren't hashable.

--- statements2csv.py
+++ statements2csv.py
@@ -74,6 +74,7 @@ def extract_dataframes(fil: str) -> Generator[Extraction, None, None]:
     extractors = (extract_applecard, extract_capitalone, extract_chase)
 
     tables = camelot.read_pdf(fil, pages="all", flavor="stream")
+    last_extraction = None
     for table in tables:
         try:
             extractor, extraction = next(
@@ -84,6 +85,14 @@ def extract_dataframes(fil: str) -> Generator[Extraction, None, None]:
         except StopIteration:
             continue
 
+        is_duplicate_extraction = last_extraction and extraction.dataframe.equals(
+            last_extraction.dataframe
+        )
+        if is_duplicate_extraction:
+            logging.info('Extractor "%s" found duplicate table. Skipping', extractor)
+            continue
+
+        last_extraction = extraction
         logging.info('Extractor "%s" found something', extractor)
         yield extraction
 
