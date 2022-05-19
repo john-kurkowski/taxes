284a4ecb44a8cacb22c4799b274df15fa14a976e
John Kurkowski <john.kurkowski@gmail.com>
2022-05-19 12:01:36 -0700

    Only use concurrency if more than 1 input
    Eases debugging. Any worker that encounters `breakpoint()` immediately raises `bdb.BdbQuit`. With this change, one can at least debug a single input file.

--- statements2csv/__main__.py
+++ statements2csv/__main__.py
@@ -9,7 +9,7 @@ import multiprocessing
 import os
 import pathlib
 import sys
-from typing import Iterable
+from typing import Sequence
 
 import click
 
@@ -18,13 +18,18 @@ from .cli import extract_file
 
 @click.command()
 @click.argument("files", nargs=-1, required=True, type=pathlib.Path)
-def main(files: Iterable[pathlib.Path]) -> None:
+def main(files: Sequence[pathlib.Path]) -> None:
     """Convert FILES bank statement PDFs to CSV on stdout."""
-    num_cores_that_hopefully_wont_max_out_machine = max(
-        multiprocessing.cpu_count() // 2, 1
-    )
-    with multiprocessing.Pool(num_cores_that_hopefully_wont_max_out_machine) as pool:
-        pool.map(extract_file, files)
+    if len(files) > 1:
+        num_cores_that_hopefully_wont_max_out_machine = max(
+            multiprocessing.cpu_count() // 2, 1
+        )
+        with multiprocessing.Pool(
+            num_cores_that_hopefully_wont_max_out_machine
+        ) as pool:
+            pool.map(extract_file, files)
+    else:
+        extract_file(files[0])
 
 
 if __name__ == "__main__":
