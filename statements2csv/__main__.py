6e723807b606aaa96140fa54bf369ab7990701e0
John Kurkowski <john.kurkowski@gmail.com>
2022-05-27 16:33:59 -0700

    Fix missing logging, when using console script or multiple input files

--- statements2csv/__main__.py
+++ statements2csv/__main__.py
@@ -20,18 +20,24 @@ from .cli import extract_file
 @click.argument("files", nargs=-1, required=True, type=pathlib.Path)
 def main(files: Sequence[pathlib.Path]) -> None:
     """Convert FILES bank statement PDFs to CSV on stdout."""
-    if len(files) > 1:
-        num_cores_that_hopefully_wont_max_out_machine = max(
-            multiprocessing.cpu_count() // 2, 1
-        )
-        with multiprocessing.Pool(
-            num_cores_that_hopefully_wont_max_out_machine
-        ) as pool:
-            pool.map(extract_file, files)
-    else:
+
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "WARNING").upper())
+
+    if len(files) <= 1:
         extract_file(files[0])
+        return
+
+    # Let through child process logging to stderr. Note on macOS, this line is
+    # considered unsafe.
+    # https://docs.python.org/3/library/multiprocessing.html#contexts-and-start-methods
+    multiprocessing.set_start_method("fork")
+
+    num_cores_that_hopefully_wont_max_out_machine = max(
+        multiprocessing.cpu_count() // 2, 1
+    )
+    with multiprocessing.Pool(num_cores_that_hopefully_wont_max_out_machine) as pool:
+        pool.map(extract_file, files)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "WARNING").upper())
     main(sys.argv[1:])
