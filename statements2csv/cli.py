8454e89d9cccde3883107e4feacc48027a835900
John Kurkowski <john.kurkowski@gmail.com>
2022-05-27 15:32:33 -0700

    Increase typecheck aggression

--- statements2csv/cli.py
+++ statements2csv/cli.py
@@ -8,7 +8,7 @@ import click
 from .extract import extract_dataframes
 
 
-def extract_file(fil: pathlib.Path):
+def extract_file(fil: pathlib.Path) -> None:
     """Convert 1 bank statement PDF to CSV on stdout."""
     extractions = extract_dataframes(fil)
     is_empty = True
