f750e96e82694802743631d12019917a10fa2dd7
John Kurkowski <john.kurkowski@gmail.com>
2023-03-14 16:40:38 -0700

    Test greptransactions command

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,14 +3,17 @@
 Keep to a minimum. https://testing.googleblog.com/2015/04/just-say-no-to-more-end-to-end-tests.html.
 """
 
+import datetime
 import os.path
 from pathlib import Path
 
+import freezegun
 import pytest
 from click.testing import CliRunner
 from syrupy import SnapshotAssertion
 from wcmatch import glob
 
+from greptransactions.__main__ import main as greptransactions
 from statements2csv.__main__ import main as statements2csv
 
 from .snapshot_extensions import secrets_directory_extension_factory
@@ -70,3 +73,31 @@ def test_statements2csv_all_files(
     assert result.exit_code == 0
     assert result.output
     assert result.output == secret_snapshot_all_pdfs
+
+
+@freezegun.freeze_time(datetime.datetime(2022, 4, 20))
+def test_greptransactions_default_year(
+    all_pdfs_input: list[str] | None,
+    capfd: pytest.CaptureFixture,
+    secret_snapshot: SnapshotAssertion,
+) -> None:
+    if all_pdfs_input is None:
+        pytest.skip("Can't test encrypted files")
+        return
+
+    pattern = "(output|tulip)"
+    result1 = CliRunner().invoke(greptransactions, pattern)
+    output1, err1 = capfd.readouterr()
+
+    result2 = CliRunner().invoke(greptransactions, ["--year", "2022", pattern])
+    output2, err2 = capfd.readouterr()
+
+    assert result1.exit_code == 0
+    assert result2.exit_code == 0
+    assert not err1
+    assert not err2
+    assert output1
+    assert output2
+    assert output1 != output2
+    assert output1 == secret_snapshot
+    assert output2 == secret_snapshot
