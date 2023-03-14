977282d0bdd32dbbae0e60347bfaabc4aa662f7d
John Kurkowski <john.kurkowski@gmail.com>
2023-03-14 01:08:57 -0700

    Test PDF paths must contain exactly 1 year

---
+++ tests/statements2csv/test_extract.py
@@ -0,0 +1,22 @@
+"""Test the extract module."""
+
+from pathlib import Path
+
+import camelot  # type: ignore[import]
+import pytest
+
+from statements2csv.extract import extract_dataframes
+
+
+def test_extract_dataframes_must_contain_one_year(
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    monkeypatch.setattr(camelot, "read_pdf", lambda *_, **__: [])
+
+    with pytest.raises(ValueError, match="possible statement years"):
+        list(extract_dataframes(Path("years") / "000" / "a.pdf"))
+
+    with pytest.raises(ValueError, match="possible statement years"):
+        list(extract_dataframes(Path("years") / "2020" / "until" / "2029" / "a.pdf"))
+
+    assert list(extract_dataframes(Path("years") / "2020" / "a.pdf")) == []
