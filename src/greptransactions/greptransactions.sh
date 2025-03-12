50dfb205dc240d1f1f20619700d0dc528789696a
John Kurkowski <john.kurkowski@gmail.com>
2025-03-12 14:49:30 -0700

    Tab delimit greptransactions output

--- src/greptransactions/greptransactions.sh
+++ src/greptransactions/greptransactions.sh
@@ -7,4 +7,5 @@ year="$1"
 pattern="$2"
 file="$3"
 rg --no-line-number --no-filename "^\s*($year)-" "$file" | # find leading year
+  xsv fmt --out-delimiter "\t" |                           # increase terminal readability, improve paste into Google Sheets
   rg --pcre2 "($pattern)"                                  # highlight only the user's pattern
